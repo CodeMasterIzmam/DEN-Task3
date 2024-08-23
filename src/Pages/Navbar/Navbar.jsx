5714b6a05aaee564d821ffa852e4a7c365a577f0
Izmam93053 <143431888+Izmam93053@users.noreply.github.com>
2024-08-23 11:43:45 +0500

    initial commit

---
+++ src/Pages/Navbar/Navbar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import Logo2 from "../../Assets/Logo2.svg";
+import LazyLoad from "react-lazyload";
+
+const Navbar = () => {
+  return (
+    <div className="navbarMain">
+      <nav className="navbar navbar-expand-lg navbar-light bgdark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+          <LazyLoad offset={100}>
+            <img style={{ maxWidth: "65px" }} alt="Image" src={Logo2} />
+            </LazyLoad>
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <Link className="nav-link" aria-current="page" to="/">
+                Home
+              </Link>
+              <Link className="nav-link " to="/about" tabIndex="-1">
+                About
+              </Link>
+              <Link className="nav-link" to="/blog">
+                Blogs
+              </Link>
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
